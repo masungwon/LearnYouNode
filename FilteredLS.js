d7fe592de7bcae8299549c75c70191581b6deb13
Sungwon Ma <sma2@swarthmore.edu>
2016-07-14 17:58:29 -0400

    donegit add FilteredLS.js ! :D :D :D

--- FilteredLS.js
+++ FilteredLS.js
@@ -1,41 +1,14 @@
-// create a program that prints a list of files in a
-// given directory, filtered by the extension of the files
+var fs = require('fs'); // long file that exports an object; this object has a lot of functions
 
-// directory name will be the first argument
-// file extension to filterby is the second argument (add a . to front)
-
-// the list of files should be printed to the console,
-// one file per line
-
-// use asynchronous I/O (the main part of this question is
-// using asynchronous function to access our file directory)
-
-var fs = require('fs');
-
-//what is "path" and where does it come from?
-
-fs.readFile(process.argv[2], function(err,data){
-  //code
-  
-  //not synchronous; can't just put in into a variable b/
-  //our javascript process isn't going to wait. it's going to keep running
-  //the script while readDir goes and reads the files
-
-  //so, when the data comes back, we won't be at that line anymore.
-  //so we need to leave directions for readDir. Essentially,
-  //"when you are done reading the file directoy, run this function"
-  // (aka callback function)
-
-  //so here, callback is the second parameter for readFile.
-  //err is going to tell you if anything went wrong while
-  //readFile was reading the direcotires, and data is the
-  //data returned if it's successful.
-
-  //first we need to handle any errors.
-
+var path=require('path');
+//console.log(process);
+fs.readdir(process.argv[2], function(err,data){
   if(err) console.error(err);
-})
-  //now we need to deal with data, which is the same data you got back
-  //from fs.readFileSync (not readDir...)
-  //So you can deal with it the same way; but anything you
-  //do with it has to be inside the callback function
+  var filteredlist = data.filter(function(file) {
+    //console.log(path.extname(file));
+    return (path.extname(file) == "." + process.argv[3]);
+  })
+  for (var i=0; i<filteredlist.length; i++) {
+    console.log(filteredlist[i]);
+  }
+});
